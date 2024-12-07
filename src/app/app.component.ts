fe899d454029a1f4dfa229a0c6b79b4d263f8e88
Shikhar <iamshikhartiwari@gmail.com>
2024-12-07 06:43:42 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
+
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  // standalone: true,
+  // imports: [RouterOutlet]
+  imports: [NavbarComponent]
 })
 export class AppComponent {
   title = 'Hexasset';
